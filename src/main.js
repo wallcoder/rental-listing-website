cce7650812bdc874db67436304cafc92ae846a38
cen1c <sbfactual@gmail.com>
2025-03-24 18:11:46 +0530

    list page added

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import './assets/main.css'
+import 'boxicons'
+import 'boxicons/css/boxicons.min.css';
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
